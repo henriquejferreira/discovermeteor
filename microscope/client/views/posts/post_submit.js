47b3e5a4c5ac37fef58a884d831696c5bed4cf4b
Henrique Ferreira <henriquejoaof@gmail.com>
2014-04-21 22:45:15 +0100

    7-6 Use a method to submit a post

--- microscope/client/views/posts/post_submit.js
+++ microscope/client/views/posts/post_submit.js
@@ -1,15 +1,18 @@
 Template.postSubmit.events({
     'submit form': function(e){
         e.preventDefault(); //prevent the normal action of the browser. eg:going to an url on a form submit
-        
+
         var post = {
             url:$(e.target).find('[name=url]').val(),
             title:$(e.target).find('[name=title]').val(),
             message:$(e.target).find('[name=message]').val()
         }
-        post._id = Posts.insert(post);
-        Router.go('postPage', post);
-        
+
+        Meteor.call('post', post, function(error,id){
+          if(error)
+            return alert(error.reason);
+
+          Router.go('postPage', post);
+        });
     }
-    
 });
